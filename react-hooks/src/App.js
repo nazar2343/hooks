f17ca26442bba537a644b10504fff313052b8a55
nazar2343 <barannazar234@gmail.com>
2022-03-18 10:35:44 +0000

    hooks react

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,37 +1,43 @@
 import './App.css';
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
-  const APP_ID = "yourData Application-ID";
-  const APP_KEY="yourData Application-Kyes";
 
-  useEffect(() => {
-    getRecipes()
-  },[]);
+  function competitionRandomNumber() {
+    console.log('Some calculations...')
+    return Math.trunc(Math.random() * 20)
+  }
 
-  const [counter, setCounter] = useState(0);
-  const [recipes, setRecipes] = useState([]);
-  useEffect(() => {
-    console.log("useEffect has been run")
-  },[counter]);
+  function updateTitle() {
+    setState((prev) => {
+      return {
+      ...prev,
+      title: 'Нова заголовок'
+    }
+  })
+  }
 
+  const [counter, setCounter] = useState(() => competitionRandomNumber())
+  const [state, setState] = useState({
+    title: 'Лічильник',
+    date: Date.now()
+  })
 
-  const getRecipes = async() => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-    const data = await response.json()
-    setRecipes(data)
-    console.log(data)
+  const increment = () => {
+    setCounter((prev) => prev + 1)
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1)
   }
 
   return (
-    <div className="App">
-      <form className = 'search-form'>
-        <input className = 'search-bar' type='text' />
-        <button className = 'search-button' type='submit'>Search</button>
-      </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+    <div>
+      <h1>Лічильник: {counter}</h1>
+      <button className='success' onClick={increment}>Додати</button>
+      <button className='danger' onClick={decrement}>Забрати</button>
+      <button className='yellow' onClick={updateTitle}>Змінити назву</button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
