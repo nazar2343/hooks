81d602fe58fd320b6e8782d7147493e5798ac002
nazar2343 <barannazar234@gmail.com>
2022-03-20 11:57:39 +0000

    save hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,25 +3,66 @@ import React, { useState, useEffect } from 'react'
 
 const App = () => {
 
-  const [resource, setResource] = useState('posts')
-  const [items, setItems] = useState([])
+  const [data, setData] = useState([])
+  const [bet, setBet] = useState([])
+  const [change, setChange] = useState('')
+
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resource}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-  }, [resource])
+    const raw = localStorage.getItem('data') || []
+    setData(JSON.parse(raw))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(data))
+  }, [data])
+
+  const getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(json => setData(json))
+  }
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${change}`)
+    .then(res => res.json())
+    .then(json => setBet(json))
+  }, [change])
+
+  useEffect(() => {
+    const war = localStorage.getItem('bet') || []
+    setBet(JSON.parse(war))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('bet', JSON.stringify(bet))
+  },[bet])
+
+  const removeData = () => {
+    setData([])
+  }
+
+  const remove = () => {
+    setBet([])
+  }
 
   return (
     <>
       <div>
-        <button onClick={() => setResource('posts')}>Posts</button>
-        <button onClick={() => setResource('users')}>Users</button>
-        <button onClick={() => setResource('comments')}>Comments</button>
+        <h1>{change}</h1>
+        <pre>{JSON.stringify(bet, null, 2)}</pre>
+        <button onClick={() => setChange('users')}>Користувачі</button>
+        <button onClick={() => setChange('comments')}>Коменти</button>
+        <button onClick={() => setChange('posts')}>Пости</button>
+        <button onClick={() => remove()}>Delete everything</button>
+
+        <pre>{JSON.stringify(data, null ,2)}</pre>
+
+        <hr />
+        <hr />
+
+        <button onClick={() => getData()}>Add from JSON</button>
+        <button onClick={() => removeData()}>Delete from JSON</button>
       </div>
-      <h1>{resource}</h1>
-      {items.map(item => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
     </>
   );
 }
--- react-hooks/src/examples(useState)/state.js
+++ react-hooks/src/examples(useState)/state.js
@@ -120,3 +120,34 @@
 // }
 
 // export default App;
+
+
+import './App.css';
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+
+  const [resource, setResource] = useState('posts')
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resource])
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('users')}>Users</button>
+        <button onClick={() => setResource('comments')}>Comments</button>
+      </div>
+      <h1>{resource}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+    </>
+  );
+}
+
+export default App;
